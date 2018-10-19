a4441878a30cfa9fa5f7edc32a3a80b5ce336440
Gabriel Khachatryan <gmkhach@gmail.com>
2018-10-18 23:17:20 -0400

    brute-force implementation

--- StringCompression/Program.cs
+++ StringCompression/Program.cs
@@ -10,6 +10,45 @@ namespace StringCompression
     {
         static void Main(string[] args)
         {
+            do
+            {
+                try
+                {
+                    Console.Write("\nEnter a test string\n\n>>> ");
+                    string input = Console.ReadLine().Trim();
+                    Console.WriteLine("\nCompressed string: " + Compress(input));
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("\n" + ex.Message);
+                }
+                Console.Write("\nPress Enter to try another string...");
+                Console.ReadLine();
+                Console.Clear();
+            } while (true);
+        }
+
+        private static string Compress(string input)
+        {
+            string output = "";
+            int counter = 1;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (i == input.Length - 1)
+                {
+                    output += $"{input[i]}{counter}";
+                }
+                else if (input[i] == input[i + 1])
+                {
+                    counter++;
+                }
+                else
+                {
+                    output += $"{input[i]}{counter}";
+                    counter = 1;
+                }
+            }
+            return output;
         }
     }
 }
